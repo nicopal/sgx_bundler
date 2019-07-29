8b05134be0c23e242281b416279410e2b4ac6750
jakob <jaksve@kth.se>
2019-07-29 11:41:53 +0200

    moved execute function method to its own c file

--- src/trusted/static_trusted/execute_function.h
+++ src/trusted/static_trusted/execute_function.h
@@ -4,16 +4,7 @@
 #include "hotcall_config.h"
 #include <stdint.h>
 
-static inline void
-execute_function(const struct hotcall_config *hotcall_config, uint8_t function_id, int n_iters, int n_params, void *args[n_params][n_iters]) {
-    void (*f)(unsigned int n_iters, unsigned int n_params, void *[n_params][n_iters]);
-    f = hotcall_config->call_table[function_id];
-    #ifdef SGX_DEBUG
-    if(!f) {
-        printf("unknown hotcall function %d.\n", function_id);
-    }
-    #endif
-    f(n_iters, n_params, args);
-}
+void
+execute_function(const struct hotcall_config *hotcall_config, uint8_t function_id, int n_iters, int n_params, void *args[n_params][n_iters]);
 
 #endif
